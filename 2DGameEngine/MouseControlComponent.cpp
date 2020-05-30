52243a65da48ff237ec79c430042ba647a6d8d11
Jake Donese <jake.donese@awh.net>
2020-05-30 16:08:29 -0400

    v

--- 2DGameEngine/MouseControlComponent.cpp
+++ 2DGameEngine/MouseControlComponent.cpp
@@ -1,14 +1,17 @@
 #include <math.h>
 #include "MouseControlComponent.h"
-
+#include <iostream>
 void MouseControlComponent::Initialize() {
 
 }
 
 void MouseControlComponent::Update(float deltaTime) {
     if(dst != nullptr && start != nullptr){
-        auto normalizedVec = sqrt((pow((dst->x - start->x), 2), pow(dst->y - start->y, 2)));
-
+        auto directionVecX = dst->x - start->x;
+        auto directionVecY = dst->y - start->y;
+        auto vecMag = sqrt((pow((dst->x - start->x), 2), pow(dst->y - start->y, 2)));
+        // divide x and y  by mag for normalized vec
+        auto normalizedVec =
     }
 }
 
